bb033f83dddede28a4e03c0c40dc839d2047b807
吴小白 <296015668@qq.com>
2024-11-06 06:43:12 +0800

    feat: 更新 v2.0.0

--- build-containerd.sh
+++ build-containerd.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 #
-CONTAINERD_VERSION=v1.7.22
+CONTAINERD_VERSION=v2.0.0
 
 ################################################################
-# REF: v1.7.22
+# REF: v2.0.0
 #
 REF=${CONTAINERD_VERSION}
 
 TMPDIR=$(mktemp -d)
 
 git clone --depth=1 https://github.com/docker/containerd-packaging "${TMPDIR}"
-cp containerd.patch "${TMPDIR}"
-
 pushd "${TMPDIR}" || exit 1
 
 ################################################################
@@ -22,13 +20,6 @@ sed -i '/syntax=docker/d' dockerfiles/deb.dockerfile
 sed -i 's@ca-certificates@ca-certificates libbtrfs-dev@g' dockerfiles/deb.dockerfile
 sed -i 's@GOLANG_IMAGE=golang@GOLANG_IMAGE=ghcr.io/loong64/golang@g' common/common.mk
 sed -i 's@ARCH=$(shell uname -m)@ARCH=loong64@g' Makefile
-
-################################################################
-# See. https://github.com/containerd/containerd
-# libcontainer/system/syscall_linux_64.go not support linux/loong64
-# vendor/github.com/cilium/ebpf not support linux/loong64
-#
-git apply containerd.patch || exit 1
 
 make REF=${REF} BUILD_IMAGE=ghcr.io/loong64/debian:trixie-slim
 
