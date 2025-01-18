bfb80a78ac012b5437ed4bf9b009703d2f8c156b
吴小白 <296015668@qq.com>
2025-01-18 15:02:56 +0800

    feat: 添加 opencloudos-23 rpm 包构建

--- build-containerd.sh
+++ build-containerd.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
 #
-
-set -e
-
 CONTAINERD_VERSION=v2.0.0
+
+BUILD_DEB=0
+BUILD_RPM=0
+
+while [[ $# > 0 ]]; do
+    lowerI="$(echo $1 | awk '{print tolower($0)}')"
+    case $lowerI in
+        -h|--help)
+            echo "Usage: $0 [OPTIONS]"
+            echo
+            echo "Global Options:"
+            echo -e "  -h, --help  \t Show this help message and exit"
+            echo -e "  --deb  \t Build Debian-trixie deb package"
+            echo -e "  --rpm  \t Build OpenCloudOS-23 rpm package"
+            exit 0
+            ;;
+        --deb)
+            BUILD_DEB=1
+            shift
+            ;;
+        --rpm)
+            BUILD_RPM=1
+            shift
+            ;;
+        *)
+            echo "install: Unknown option $1"
+            echo "eg: $0 --deb --rpm"
+            exit 1
+            ;;
+    esac
+    shift
+done
 
 ################################################################
 # REF: v2.0.0
@@ -15,13 +44,14 @@ TMPDIR=$(mktemp -d)
 git clone --depth=1 https://github.com/docker/containerd-packaging "${TMPDIR}"
 
 cp -f containerd.patch /tmp/containerd.patch
+cp -f opencloudos-23/Dockerfile "${TMPDIR}/dockerfiles/rpm.dockerfile"
 
 pushd "${TMPDIR}" || exit 1
 ################################################################
 # See. https://hub.docker.com/r/docker/dockerfile/tags
 # docker.io/docker/dockerfile not support linux/loong64
 #
-sed -i '/syntax=docker/d' dockerfiles/deb.dockerfile
+# sed -i '/syntax=docker/d' dockerfiles/deb.dockerfile
 sed -i 's@GOLANG_IMAGE=golang@GOLANG_IMAGE=ghcr.io/loong64/golang@g' common/common.mk
 sed -i 's@ARCH=$(shell uname -m)@ARCH=loong64@g' Makefile
 
@@ -37,11 +67,21 @@ sed -i 's@ARCH=$(shell uname -m)@ARCH=loong64@g' Makefile
 
 git apply /tmp/containerd.patch || exit 1
 
-make REF=${REF} BUILD_IMAGE=ghcr.io/loong64/debian:trixie-slim
+if [ "${BUILD_DEB}" = '1' ]; then
+    make REF=${REF} BUILD_IMAGE=ghcr.io/loong64/debian:trixie-slim
+fi
+if [ "${BUILD_RPM}" = '1' ]; then
+    make REF=${REF} BUILD_IMAGE=ghcr.io/loong64/opencloudos:23
+fi
 
 popd || exit 1
 
 mkdir -p dist
-mv ${TMPDIR}/build/debian/trixie/loong64/* dist/
+if [ "${BUILD_DEB}" = '1' ]; then
+    mv ${TMPDIR}/build/debian/trixie/loong64/* dist/
+fi
+if [ "${BUILD_RPM}" = '1' ]; then
+    mv ${TMPDIR}/build/opencloudos/23/loongarch64/* dist/
+fi
 
 rm -rf "${TMPDIR:?}"
